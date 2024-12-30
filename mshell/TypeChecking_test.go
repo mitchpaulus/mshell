11aebc6b93a5d65e5a65d7f9ff310d3dad8500be
Mitchell Paulus <paulusm14@gmail.com>
2024-12-30 13:27:10 -0600

    Add Generic type binding, unit tests to CI (#17)

---
+++ mshell/TypeChecking_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	// "fmt"
+	// "strings"
+	"testing"
+)
+
+func TestSimpleBinding(t *testing.T) {
+	generic := TypeGeneric{ "a" }
+	concrete := TypeInt{}
+
+	binding, err := generic.Bind(concrete)
+	if err != nil {
+		t.Error("Binding failed")
+	}
+
+	if len(binding) != 1 {
+		t.Error("Binding failed")
+	}
+
+	if !binding[0].Type.Equals(concrete) {
+		t.Error("Binding failed")
+	}
+}
+
+func TestRecursiveBinding(t *testing.T) {
+	generic := &TypeList{ 
+		ListType: &TypeList {
+			ListType: TypeGeneric{ "a" },
+			Count: -1,
+		},
+		Count: -1,
+	}
+
+	concrete := &TypeList{
+		ListType: &TypeList{
+			ListType: TypeInt{},
+			Count: -1,
+		},
+		Count: -1,
+	}
+
+	binding, err := generic.Bind(concrete)
+	if err != nil {
+		t.Error("Binding failed")
+	}
+
+	if len(binding) != 1 {
+		t.Error("Binding failed")
+	}
+
+	if !binding[0].Type.Equals(TypeInt{}) {
+		t.Error("Binding failed")
+	}
+}
