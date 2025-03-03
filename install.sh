e0a8aaa010bcd8c304bc1ec254254db7f1c51fad
Mitchell Paulus <paulusm14@gmail.com>
2025-03-03 10:38:51 -0600

    Add simple install script for teammates
    External persons should not use.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p "$HOME"/.local/bin
+curl -sL 'https://github.com/mitchpaulus/mshell/releases/download/latest/linux_amd64.tar.gz' | tar -xz -C "$HOME"/.local/bin
+
+# Check for symlink, if it doesn't exist, create it
+ln -s "$HOME"/.local/bin/msh "$HOME"/.local/bin/mshell 2>/dev/null
+
+mkdir -p "$HOME"/.local/share/mshell
+curl -sL 'https://raw.githubusercontent.com/mitchpaulus/mshell/refs/heads/main/lib/std.msh' > "$HOME"/.local/share/mshell/std.msh
+# grep for 'export MSHSTDLIB' in .bashrc, if it doesn't exist, add it
+
+# shellcheck disable=SC2016
+grep -q 'export MSHSTDLIB' "$HOME"/.bashrc || \
+    echo 'export MSHSTDLIB="$HOME/.local/share/mshell/std.msh"' >> "$HOME"/.bashrc
