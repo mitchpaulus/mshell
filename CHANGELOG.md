e823e798418e5ed1db77f3ac74ce598db39e8c44
Mitchell Paulus <paulusm14@gmail.com>
2026-02-13 10:44:01 -0600

    Include standard lib in releases, update release CI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -33,6 +33,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
+- Builds/releases now are pure Go, built with `CGO_ENABLED=0`.
+
 ## 0.9.0 - 2026-01-27
 
 ### Added
--- install.sh
+++ install.sh
@@ -3,16 +3,19 @@
 # Can use this script like:
 # curl -sL https://raw.githubusercontent.com/mitchpaulus/mshell/refs/heads/main/install.sh | sh -
 
+DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/mshell"
+
 mkdir -p "$HOME"/.local/bin
+mkdir -p "$DATA_DIR"
 curl -sL 'https://github.com/mitchpaulus/mshell/releases/latest/download/linux_amd64.tar.gz' | tar -xz -C "$HOME"/.local/bin
 
 # Check for symlink, if it doesn't exist, create it
 ln -s "$HOME"/.local/bin/msh "$HOME"/.local/bin/mshell 2>/dev/null
 
-mkdir -p "$HOME"/.local/share/mshell
-curl -sL 'https://raw.githubusercontent.com/mitchpaulus/mshell/v0.7.0/lib/std.msh' > "$HOME"/.local/share/mshell/std.msh
+# Move std.msh from the release tarball to the share directory
+mv "$HOME"/.local/bin/std.msh "$DATA_DIR"/std.msh
 # grep for 'export MSHSTDLIB' in .bashrc, if it doesn't exist, add it
 
 # shellcheck disable=SC2016
 grep -q 'export MSHSTDLIB' "$HOME"/.bashrc || \
-    echo 'export MSHSTDLIB="$HOME/.local/share/mshell/std.msh"' >> "$HOME"/.bashrc
+    echo 'export MSHSTDLIB="${XDG_DATA_HOME:-$HOME/.local/share}/mshell/std.msh"' >> "$HOME"/.bashrc
