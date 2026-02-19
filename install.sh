8c7f99dcc1e6e1b24b850b3221365a921560394f
Mitchell Paulus <paulusm14@gmail.com>
2026-02-19 13:54:43 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -5,9 +5,30 @@
 
 DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/mshell"
 
+# Detect OS
+OS="$(uname -s)"
+case "$OS" in
+    Linux)  GOOS="linux" ;;
+    Darwin) GOOS="darwin" ;;
+    *)      echo "Unsupported OS: $OS" >&2; exit 1 ;;
+esac
+
+# Detect CPU architecture and map to Go's naming convention
+ARCH="$(uname -m)"
+case "$ARCH" in
+    x86_64)        GOARCH="amd64" ;;
+    i386|i686)     GOARCH="386" ;;
+    armv6l|armv7l) GOARCH="arm" ;;
+    aarch64|arm64) GOARCH="arm64" ;;
+    *)             echo "Unsupported architecture: $ARCH" >&2; exit 1 ;;
+esac
+
+TARBALL="${GOOS}_${GOARCH}.tar.gz"
+echo "Detected platform: $GOOS/$GOARCH (tarball: $TARBALL)"
+
 mkdir -p "$HOME"/.local/bin
 mkdir -p "$DATA_DIR"
-curl -sL 'https://github.com/mitchpaulus/mshell/releases/latest/download/linux_amd64.tar.gz' | tar -xz -C "$HOME"/.local/bin
+curl -sL "https://github.com/mitchpaulus/mshell/releases/latest/download/${TARBALL}" | tar -xz -C "$HOME"/.local/bin
 
 # Check for symlink, if it doesn't exist, create it
 ln -s "$HOME"/.local/bin/msh "$HOME"/.local/bin/mshell 2>/dev/null
@@ -16,6 +37,11 @@ ln -s "$HOME"/.local/bin/msh "$HOME"/.local/bin/mshell 2>/dev/null
 mv "$HOME"/.local/bin/std.msh "$DATA_DIR"/std.msh
 # grep for 'export MSHSTDLIB' in .bashrc, if it doesn't exist, add it
 
+# Add MSHSTDLIB export to the appropriate shell rc file
 # shellcheck disable=SC2016
-grep -q 'export MSHSTDLIB' "$HOME"/.bashrc || \
-    echo 'export MSHSTDLIB="${XDG_DATA_HOME:-$HOME/.local/share}/mshell/std.msh"' >> "$HOME"/.bashrc
+MSHSTDLIB_EXPORT='export MSHSTDLIB="${XDG_DATA_HOME:-$HOME/.local/share}/mshell/std.msh"'
+if [ "$GOOS" = "darwin" ] && [ -f "$HOME/.zshrc" ]; then
+    grep -q 'export MSHSTDLIB' "$HOME"/.zshrc || echo "$MSHSTDLIB_EXPORT" >> "$HOME"/.zshrc
+else
+    grep -q 'export MSHSTDLIB' "$HOME"/.bashrc || echo "$MSHSTDLIB_EXPORT" >> "$HOME"/.bashrc
+fi
