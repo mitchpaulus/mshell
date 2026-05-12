5565e7a0617432b9211296c3184fa4db391dccb5
Mitchell Paulus <paulusm14@gmail.com>
2026-05-11 22:03:57 -0500

    Move tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-find . -maxdepth 1 -name '*.msh' | sort -V | parallel -k ./test_file.sh
+cd success || exit 1
+find . -maxdepth 1 -name '*.msh' | sort -V | parallel -k ../test_file.sh
+
+cd ../fail || exit 1
+find . -maxdepth 1 -name '*.msh' | sort -V | parallel -k ../test_file.sh
